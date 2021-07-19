9e96646e48c2f19d130e9374be532aec5d3e041a
Lilieyen <lnyasita@gmail.com>
2021-07-19 04:42:32 +0300

    use .then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-	const body = { status: 200, body: 'Success' };
-	return promise
-		.then(() => body)
-		.catch(() => new Error())
-		.finally(() => {
-			console.log('Got a response from the API');
-		});
+    const body = { status: 200, body: 'success' };
+  
+    return promise
+      .then(() => body)
+      .catch(() => new Error())
+      .finally(() => {
+        console.log('Got a response from the API');
+    });
 }
