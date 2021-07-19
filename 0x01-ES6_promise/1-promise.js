745cce5744e4c7d3221b94dcb703b8cbaa32b112
Lilieyen <lnyasita@gmail.com>
2021-07-19 04:16:41 +0300

    promise boolean

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function getFullResponseFromAPI(success) {
+	return new Promise((resolve, reject) => {
+		if true
+		resolve({
+			status: 200
+		body: 'Success'})
+		if false
+		reject(The fake API is not working currently)
