5b24a2604221734fc88d0a7db1acadef3c68a392
Lilieyen <lnyasita@gmail.com>
2021-07-22 01:46:55 +0300

    get Ids

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,6 +3,8 @@ export default function getListStudentIds(arg) {
 	if (!Array.isArray(arg)) {
 		return [];
 	}
-	return arg.map(item) => item.id;
+	return arg.map((item) => {
+		return item.id;
+	});
 };
 
