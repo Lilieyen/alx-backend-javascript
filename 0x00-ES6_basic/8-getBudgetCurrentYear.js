6ad36a857c5b5c196186ec94e8219865c4b99955
Lilieyen <lnyasita@gmail.com>
2021-07-25 15:20:00 +0300

    computed property names

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+  income,
+  gdp,
+  capita
+  };
+
+  return budget;
+}
+
