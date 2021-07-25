92c1f5c94bc815e08bb6bac2b3f20656911f7824
Lilieyen <lnyasita@gmail.com>
2021-07-25 15:22:41 +0300

    computed property names

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,9 +1,4 @@
 /* eslint-disable */
-function getCurrentYear() {
-  const date = new Date();
-  return date.getFullYear();
-}
-
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
   income,
